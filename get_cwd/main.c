472fd9b9a77c884c3520169f8a48318b48d76006
dmitry <dimason99@gmail.com>
2020-02-25 20:10:06 +0600

    get_cwd working right and counting links

--- get_cwd/main.c
+++ get_cwd/main.c
@@ -14,26 +14,41 @@ struct node {
 
 int get_cwd(char* buffer, size_t size) {
 
-    struct stat *_stat = malloc(sizeof(struct stat));
+    struct stat* _stat = malloc(sizeof(struct stat));
     stat("/", _stat);
     __ino_t _root_ino = _stat->st_ino;
+    __dev_t _root_dev = _stat->st_dev;
 
-    struct node* _cur_node;
+    stat(".", _stat);
+    __ino_t _start_ino = _stat->st_ino;
+    __dev_t _start_dev = _stat->st_dev;
+    int _link_count = 0;
 
-
+    struct node* _cur_node = NULL;
+    __ino_t _cur_ino;
+    __dev_t _cur_dev;
+    char _str[MAX_FILE_NAME] = "";
     while(1) {
         stat(".", _stat);
-        if(_stat->st_ino == _root_ino) {
+        _cur_ino = _stat->st_ino;
+        _cur_dev = _stat->st_dev;
+        if(_cur_ino == _root_ino && _cur_dev == _root_dev) {
             break;
         }
         DIR* _dir = opendir("..");
-        struct dirent* _dirent = malloc(sizeof(struct dirent));
+        struct dirent* _dirent;
         while((_dirent = readdir(_dir)) != NULL) {
-            if(_dirent->d_ino == _stat->st_ino) {
+            strcpy(_str, "../");
+            strcat(_str, _dirent->d_name);
+            stat(_str, _stat);
+            if(_stat->st_ino == _cur_ino && _stat->st_dev == _cur_dev) {
                 struct node* _node = malloc(sizeof(struct node));
                 _node->prev = _cur_node;
                 strcpy(_node->name, _dirent->d_name);
                 _cur_node = _node;
+            }
+            if(_stat->st_ino == _start_ino && _stat->st_dev == _start_dev) {
+                _link_count++;
             }
         }
         chdir("..");
@@ -49,12 +64,14 @@ int get_cwd(char* buffer, size_t size) {
         free(_old_node);
     }
 
-    return 0;
+    free(_stat);
+
+    return _link_count;
 
 }
 
 int main() {
-    size_t BUFFER_SIZE = 128;
+    size_t BUFFER_SIZE = 256;
     char* buffer = malloc(BUFFER_SIZE);
 
     /*getcwd(buffer, BUFFER_SIZE);
@@ -66,8 +83,9 @@ int main() {
     stat("..", result);
     printf("%ld",result->st_ino);*/
 
-    get_cwd(buffer, BUFFER_SIZE);
+    int link_count = get_cwd(buffer, BUFFER_SIZE);
     printf("%s\n", buffer);
+    printf("%i\n", link_count);
     return 0;
 }
 
